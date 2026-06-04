5bf1ac72897b8a9b88fcfe82bff8d84d8b94dc62
samia2302 <meghefg@gmail.com>
2026-06-04 21:54:38 +0600

    setup

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,24 @@
-const express = require('express')
-const app = express()
+import express, { type Application, type Request, type Response } from "express"
+import {Pool} from "pg"
+
+const app: Application = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({extended: true}))
+
+
+const pool = new Pool({
+    connectionString: "postgresql://neondb_owner:npg_hbrPCa9cEKV4@ep-billowing-union-aqcmbd7b-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+})
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({
+    "message": "express server",
+    "author": "next level"
+  })
 })
 
 app.listen(port, () => {
